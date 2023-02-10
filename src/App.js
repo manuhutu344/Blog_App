67f1f2ec0c05930e2e4ef4240b0bbc1a8d0c1fe9
manuhutu344 <nandoperdos2@gmail.com>
2023-02-10 09:08:53 +0900

    Menambahkan div dengan classname post, image dan texts. Serta memasukan gambar dan juga h2 sebagai judul dan p sebagai isi blognya

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
     <a href=''>Register</a>
     </nav>
     </header>
+    <div className='post'>
+    <div className='image'>
+    <img src='https://cdn.gamebrott.com/wp-content/uploads/2023/02/Gameplay-Resident-Evil-4-Remake-Header.jpg' alt='' />
+    </div>
+    <div className='texts'>
+    <h2>Residen Evil Nich</h2>
+    <p>Resident Evil adalah game buatan capcom, yang menjadi game yang banyak dimainkan.</p>
+    </div>
+    </div>
     </main>
   );
 }
