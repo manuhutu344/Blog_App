2afce7ace84e9b6222c7543615824aa9c9ae8829
manuhutu344 <nandoperdos2@gmail.com>
2023-02-13 09:31:42 +0900

    Merubah susunan routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import Post from './componen/Post';
-import Nav from './componen/Nav';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './componen/Layout';
 
 function App() {
   return (
     <Routes>
-    <Route index element={
-      <main>
-    <Nav />
-    <Post />
-    <Post />
-    <Post />
-    </main>
-    } />
+    <Route path='/' element={<Layout />}>
+    <Route index element={<Post />} />
+    <Route path={'/login'} element={<div>Halaman Login</div>} />
+    </Route> 
     </Routes>
   );
 }
