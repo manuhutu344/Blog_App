f42ae1b207fdf19065d592ce2e2cae2d46c42d6f
manuhutu344 <nandoperdos2@gmail.com>
2023-02-14 08:29:47 +0900

    Membuat Login Page

---
+++ src/page/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+    <form action=''>
+    <input type='text' placeholder='Masukan Username Anda' />
+    <input type='password' placeholder='Masukan Password Andas' />
+    <button>Login</button>
+    </form>
+    </div>
+  )
+}
+
+export default Login
