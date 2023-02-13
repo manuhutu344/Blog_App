cc23844c1058c30ca3856228b554d4dc4d712561
manuhutu344 <nandoperdos2@gmail.com>
2023-02-13 09:14:08 +0900

    Menambahkan route dan routes react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Post from './componen/Post';
 import Nav from './componen/Nav';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
+    <Routes>
+    <Route index element={
+      <main>
     <Nav />
     <Post />
     <Post />
     <Post />
     </main>
+    } />
+    </Routes>
   );
 }
 
