5ed0eb54e88882bc9148f2e3d00c24f6e6cee147
manuhutu344 <nandoperdos2@gmail.com>
2023-02-14 08:29:23 +0900

    Menambahkan Login Page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './componen/Layout';
 import Home from './page/Home';
+import Login from './page/Login';
 
 function App() {
   return (
     <Routes>
     <Route path='/' element={<Layout />}>
     <Route index element={<Home />} />
-    <Route path={'/login'} element={<div>Halaman Login</div>} />
+    <Route path={'/login'} element={<Login />} />
     </Route> 
     </Routes>
   );
