274eb9b9851f3fef8915da4c2ea742204fc6f1f1
manuhutu344 <nandoperdos2@gmail.com>
2023-02-14 08:36:15 +0900

    Membuat Page Register

---
+++ src/page/Register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+    <form action=''>
+    <input type='text' placeholder='Masukan Username Anda' />
+    <input type='password' placeholder='Masukan Password Andas' />
+    <button>Daftar</button>
+    </form>
+    </div>
+  )
+}
+
+export default Register
