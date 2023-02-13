1a29acae20c05393bd55e076e52911796f248ab4
manuhutu344 <nandoperdos2@gmail.com>
2023-02-14 08:35:16 +0900

    Menambahkan route ke daftar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './componen/Layout';
 import Home from './page/Home';
 import Login from './page/Login';
+import Register from './page/Register';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Route path='/' element={<Layout />}>
     <Route index element={<Home />} />
     <Route path={'/login'} element={<Login />} />
+    <Route path={'/daftar'} element={<Register />} />
     </Route> 
     </Routes>
   );
