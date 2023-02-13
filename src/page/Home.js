bca8b0a9fb982fb0c50d193bfb453d8449c4fb2b
manuhutu344 <nandoperdos2@gmail.com>
2023-02-14 08:21:53 +0900

    Membuat page untuk home menampung componen post

---
+++ src/page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from '../componen/Post'
+
+function Home() {
+  return (
+    <div>
+    <Post />
+    <Post />
+    <Post />
+    </div>
+  )
+}
+
+export default Home
