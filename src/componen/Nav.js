d2cd447899d2a2b9ba6f9d51c92af5077d53ae7c
manuhutu344 <nandoperdos2@gmail.com>
2023-02-13 09:09:06 +0900

    Membuat componen nav

---
+++ src/componen/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <header>
+    <a href='' className='logo'>Blog Saya</a>
+    <nav>
+    <a href=''>Login</a>
+    <a href=''>Register</a>
+    </nav>
+    </header>
+  )
+}
+
+export default Nav
