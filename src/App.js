84139cc1d76ec4d682fccfc0e34670f1a7bfaefb
manuhutu344 <nandoperdos2@gmail.com>
2023-02-14 08:22:19 +0900

    Menambahkan page home menjadi index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Post from './componen/Post';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './componen/Layout';
+import Home from './page/Home';
 
 function App() {
   return (
     <Routes>
     <Route path='/' element={<Layout />}>
-    <Route index element={<Post />} />
+    <Route index element={<Home />} />
     <Route path={'/login'} element={<div>Halaman Login</div>} />
     </Route> 
     </Routes>
