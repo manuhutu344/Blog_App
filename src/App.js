190deba35109b19085610d9a5bc7798e4d9a47b6
manuhutu344 <nandoperdos2@gmail.com>
2023-02-13 09:04:17 +0900

    Menambahkan componen post

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Post from './componen/Post';
 
 function App() {
   return (
@@ -10,21 +11,7 @@ function App() {
     <a href=''>Register</a>
     </nav>
     </header>
-    <div className='post'>
-    <div className='image'>
-    <img src='https://cdn.gamebrott.com/wp-content/uploads/2023/02/Gameplay-Resident-Evil-4-Remake-Header.jpg' alt='' />
-    </div>
-    <div className='texts'>
-    <h2>Residen Evil Nich</h2>
-    <p className='info'>
-    <a className='author'>
-    Penulis
-    </a>
-    <time>2023-02-13 8:32</time>
-    </p>
-    <p className='summary'>Resident Evil adalah game buatan capcom, yang menjadi game yang banyak dimainkan.</p>
-    </div>
-    </div>
+    <Post />
     </main>
   );
 }
