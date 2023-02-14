67e2c27687bffdbc4b8c7dab088f39a95cd99570
manuhutu344 <nandoperdos2@gmail.com>
2023-02-15 08:03:33 +0900

    Menambahkan Nodemon dan menambahkan res

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/coba', (req, res)=>{
+    res.json('Berhasil')
+})
+
 app.listen(9001, ()=>{
     console.log('Terkoneksi ke Server')
 })
