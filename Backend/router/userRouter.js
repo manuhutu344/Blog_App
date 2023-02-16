6faa01b375e0da9dd04e82747682ed048a680773
manuhutu344 <nandoperdos2@gmail.com>
2023-02-16 19:00:15 +0900

    Memasukan data ke database mongodb

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -3,6 +3,10 @@ const User = require('../Models/User')
 
 router.post('/register', async(req, res)=>{
     const {username, password} = req.body
-    res.json({requestData:{username, password}})
+    const userDoc = await User.create({
+        username,
+        password
+    })
+    res.json(userDoc)
 })
 module.exports = router
