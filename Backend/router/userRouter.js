66c36cdd8a71f2b7fffa62f46a03f6f2e4e1f5f5
manuhutu344 <nandoperdos2@gmail.com>
2023-02-16 18:50:21 +0900

    Menghubungkan dengan mongodb

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
+const User = require('../Models/User')
 
 router.post('/register', async(req, res)=>{
     const {username, password} = req.body
     res.json({requestData:{username, password}})
 })
-
 module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const userRouter = require('./router/userRouter')
+require('dotenv').config()
+require('./connection')
 const cors = require('cors')
 
 app.use(cors())
