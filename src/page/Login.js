9ae0b60a0f21fb74ba84d5d45939062c40257749
manuhutu344 <nandoperdos2@gmail.com>
2023-02-17 22:39:00 +0900

    memasang function login

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const[username, setUsername] = useState('')
+  const[password, setPassword] = useState('')
+  async function login(e){
+    e.preventDefault()
+    await fetch('http://localhost:9001/user/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers:{'Content-Type':'application/json'}
+    })
+  }
   return (
     <div>
-    <form className='login'>
+    <form className='login' onSubmit={login}>
     <h1>Login</h1>
-    <input type='text' placeholder='Masukan Username Anda' />
-    <input type='password' placeholder='Masukan Password Andas' />
+    <input type='text' placeholder='Masukan Username Anda' value={username} onChange={e => setUsername(e.target.value)} />
+    <input type='password' placeholder='Masukan Password Andas' value={password} onChange={e => setPassword(e.target. value)} />
     <button>Login</button>
     </form>
     </div>
