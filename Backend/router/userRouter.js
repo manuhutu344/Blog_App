91d6c2cbf0d9527e7c2ef1e8a06172863a2bf1b0
manuhutu344 <nandoperdos2@gmail.com>
2023-02-17 22:05:24 +0900

    Membuat try catch di userrouter

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -3,10 +3,14 @@ const User = require('../Models/User')
 
 router.post('/register', async(req, res)=>{
     const {username, password} = req.body
-    const userDoc = await User.create({
-        username,
-        password
-    })
-    res.json(userDoc)
+    try {
+        const userDoc = await User.create({
+            username,
+            password
+        })
+        res.json(userDoc)
+    } catch (error) {
+        res.status(400).json(error)
+    }
 })
 module.exports = router
