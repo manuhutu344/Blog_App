2f5bdc4a732d4d0dd6e3940a9c43d69429b019f2
manuhutu344 <nandoperdos2@gmail.com>
2023-02-17 22:38:36 +0900

    Memasang bcrypt di register backend

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -1,15 +1,18 @@
 const router = require('express').Router()
 const User = require('../Models/User')
+const bcrypt = require('bcrypt')
+const salt = bcrypt.genSaltSync(10)
 
 router.post('/register', async(req, res)=>{
     const {username, password} = req.body
     try {
         const userDoc = await User.create({
             username,
-            password
+            password:bcrypt.hashSync(password, salt)
         })
         res.json(userDoc)
     } catch (error) {
+        console.log(error)
         res.status(400).json(error)
     }
 })
