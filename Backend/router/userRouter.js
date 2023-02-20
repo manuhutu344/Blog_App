dd49b917d5e6c0229a5b652876ae78462cae4fe3
manuhutu344 <nandoperdos2@gmail.com>
2023-02-20 19:39:07 +0900

    membuat token jwt

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const User = require('../Models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const salt = bcrypt.genSaltSync(10)
+const secret = 'qwertyuiopasdfghjklzxcvbnm1234567890'
 
 router.post('/register', async(req, res)=>{
     const {username, password} = req.body
@@ -21,7 +23,14 @@ router.post('/login', async(req, res)=>{
     const {username, password} = req.body
     const userDoc = await User.findOne({username})
    const ok = bcrypt.compareSync(password, userDoc.password)
-   res.json(ok)
+   if(ok){
+    jwt.sign({username, id:userDoc._id}, secret, {}, (err, token)=>{
+        if(err) throw err
+        res.cookie('token', token).json('bisa')
+    })
+   }else{
+    res.status(400).json('error di router post login')
+   }
 })
 
 module.exports = router
