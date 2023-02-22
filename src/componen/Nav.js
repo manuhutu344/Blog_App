f7682ac7183e923dfb0d9f3ef33cfd8b3f54a478
manuhutu344 <nandoperdos2@gmail.com>
2023-02-22 09:40:07 +0900

    membuat userinfo dan function logout

--- src/componen/Nav.js
+++ src/componen/Nav.js
@@ -1,18 +1,41 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { json, Link } from 'react-router-dom'
 
 function Nav() {
+  const [username, setUsername] = useState(null)
   useEffect(()=>{
     fetch('http://localhost:9001/user/profile', {
       credentials: 'include',
+    }).then(response =>{
+      response.json().then(userInfo =>{
+        setUsername(userInfo.username)
+      })
     })
   },[])
+
+  function logout(){
+    fetch('http://localhost:9001/user/logout', {
+      credentials: 'include',
+      method:'POST',
+    })
+  }
+
   return (
     <header>
     <Link to={'/'} className='logo'>Blog Saya</Link>
     <nav>
+    {username && (
+      <>
+      <Link to='/buat'>Buat post baru</Link>
+      <a onClick={logout}>Logout</a>
+      </>
+    )}
+    {!username &&(
+      <>
     <Link to={'/login'}>Login</Link>
     <Link to={'/daftar'}>Daftar</Link>
+      </>
+    )}
     </nav>
     </header>
   )
