4083626d71632819a21e09689c9bc51250b75826
manuhutu344 <nandoperdos2@gmail.com>
2023-02-24 22:05:20 +0900

    Membuat CreatePost

--- src/page/CreatePost.js
+++ src/page/CreatePost.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
+const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' },
+      ],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
 
 function CreatePost() {
+    const [title, setTitle] = useState('')
+    const [summary, setSummary] = useState('')
+    const [content, setContent] = useState('')
   return (
-    <div>Disini Tempat Membuat Post Baru</div>
+    <div>
+    <form>
+    <input type='title' placeholder='Masukan Judul Yang Anda Inginkan' />
+    <input type= 'summary' placeholder='Masukan Ringkasan Dari Konten Anda' />
+    <input type= 'file' />
+    <ReactQuill value={content} modules={modules} formats={formats} />
+    <button style={{marginTop:'5px'}}>Buat</button>
+    </form>
+    </div>
   )
 }
 
