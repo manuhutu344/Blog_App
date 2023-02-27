1539b29a01c883947daebe4e37fa9a392b1fe7a7
manuhutu344 <nandoperdos2@gmail.com>
2023-02-27 20:16:11 +0900

    Membuat agar file foto disimpan berdasarkan extends foto

--- Backend/router/PostRouter.js
+++ Backend/router/PostRouter.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 const multer = require('multer')
 const uploadMiddleware = multer({dest: 'uploads/'})
+const fs = require('fs')
 
 
 router.post('/post', uploadMiddleware.single('file'), (req, res)=>{
-    res.json({files:req.file})
+    const {originalname, path} = req.file
+    const parts = originalname.split('.')
+    const ext = parts[parts.length - 1]
+    const newPath = path+'.'+ext
+    fs.renameSync(path, newPath)
+    
+    res.json({ext})
 })
 
 module.exports = router
