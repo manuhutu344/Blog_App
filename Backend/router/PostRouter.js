7d91145a470b7f879bd67b4a3d3ea3b42b41704b
manuhutu344 <nandoperdos2@gmail.com>
2023-02-27 20:36:12 +0900

    Berhasil Mengupload data ke database

--- Backend/router/PostRouter.js
+++ Backend/router/PostRouter.js
@@ -2,16 +2,23 @@ const router = require('express').Router()
 const multer = require('multer')
 const uploadMiddleware = multer({dest: 'uploads/'})
 const fs = require('fs')
+const Post = require('../Models/Post')
 
 
-router.post('/post', uploadMiddleware.single('file'), (req, res)=>{
+router.post('/post', uploadMiddleware.single('file'), async(req, res)=>{
     const {originalname, path} = req.file
     const parts = originalname.split('.')
     const ext = parts[parts.length - 1]
     const newPath = path+'.'+ext
     fs.renameSync(path, newPath)
-    
-    res.json({ext})
+    const {title, summary, content} = req.body
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+    })
+    res.json(postDoc)
 })
 
 module.exports = router
