fec351fc2146f0c0b687a2d781a6f0cbe9147e19
manuhutu344 <nandoperdos2@gmail.com>
2023-03-03 20:10:22 +0900

    Membuat Tampilan artikelnya

--- src/page/Post.js
+++ src/page/Post.js
@@ -1,3 +1,4 @@
+import { formatISO9075 } from 'date-fns'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -14,10 +15,12 @@ function Post() {
     if(!postInfo) return ''
   return (
     <div className='post-page'>
+    <h1>{postInfo.title}</h1>
+    <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+    <div className='author'>Ditulis Oleh {postInfo.author.username}</div>
     <div className='image'>
     <img src={`http://localhost:9001/${postInfo.cover}`} alt='' />
     </div>
-    <h1>{postInfo.title}</h1>
     <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
   )
